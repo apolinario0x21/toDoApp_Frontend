9f84904a837587e1a0effda8c8c1b7205ba6487f
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-04-29 08:48:04 -0300

    vafeat: lidação de erros

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,16 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {Button, Container, Form, ListGroup} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const API_URL = "http://localhost:8080/api/tasks";
 
 const TaskList = () => {
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
-    /*quando for renderizado o componente, faz uma requisição para a API*/
     useEffect(() => {
         fetchTasks();
     }, []) /*[], executa apenas uma vez quando o componente é montado*/
@@ -26,7 +28,19 @@ const TaskList = () => {
                 setNewTask("");
                 fetchTasks();
             })
-            .catch(error => console.error("Erro ao adicionar tarefa: ", error));
+            .catch(error => {
+                if (error.response && error.response.data) {
+
+                    const data = error.response.data;
+                    const message = typeof data === 'string'
+                        ? data
+                        : Object.values(data).join(", ");
+                    setErrorMessage(message);
+                } else {
+                    setErrorMessage("Erro inesperado ao adicionar tarefa.");
+                }
+                console.error("Erro ao adicionar tarefa: ", error);
+            });
     }
 
     const deleteTask = (id) => {
@@ -37,42 +51,59 @@ const TaskList = () => {
 
     return (
         <Container className={"mt-5"}>
-            <h2 className={"mt-0"}>Lista de Tarefas</h2>
+            <h2 className={"mb-4"}>Lista de Tarefas</h2>
 
-            <Form className="d-flex mb-3">
-                <Form.Control
+            <Form className="d-flex flex-column mb-3">
+                <Form.Control className={"mb-2"}
                     type="text"
                     placeholder="Nova tarefa"
                     value={newTask}
-                    onChange={(e) => setNewTask(e.target.value)}
+                    onChange={(e) => {
+                        setNewTask(e.target.value);
+                        setErrorMessage("");
+                    }}
                 />
+
+                {errorMessage && (
+                    <div className="text-danger mt-2">{errorMessage}</div>
+                )}
 
                 <Button variant="primary" onClick={addTask} className={"ms-2"}>Adicionar</Button>
             </Form>
 
-            <ListGroup>
+            <ListGroup className={""}>
                 {tasks.map(task => (
-                    <ListGroup.Item key={task.id} className={"d-flex justify-content-between align-items-center"}>
-                        <div className={"bg-white"}>
-                            <span className={"fw-bold"}>{task.title}</span>
+                    <ListGroup.Item key={task.id} className={"rounded shadow-sm p-3 mb-2 bg-light text-dark"}>
+                        <div className={"d-flex flex-column"}>
 
-                            <p className={"mb-0 small text-muted"}>
-                                <strong>ID: </strong>{task.id}<br/>
-                                <strong>Criada em: </strong> {new Date(task.createdAt).toLocaleDateString()}
-                            </p>
+                            <span className={"mb-1 small text-muted"}>UUID: <code>{task.id}</code></span>
 
-                            <p className={"mb-0 small"}>{task.completed ? "Concluída" : "Pendente"}</p>
-                            <Button variant="danger" onClick={() => deleteTask(task.id)}
-                                    className={"mt-2"}>Deletar</Button>
+                            <span className={"mb-1 text-muted small"}>
+                                Criada em: {
+                                new Date(task.createdAt).toLocaleDateString("pt-BR", {
+                                    day: "2-digit",
+                                    month: "2-digit",
+                                    year: "numeric",
+                                    hour: "2-digit",
+                                    minute: "2-digit",
+                                    second: "2-digit",
+                                })
+                            }
+                            </span>
+
+                            <span className={"fw-bolder text-capitalize mb-1"}>{task.title}</span>
+                            <span className={"small mb-2"}>Status: {task.completed ? "Concluída" : "Pendente"}</span>
+                            <div>
+                                <Button variant="danger" size={"sim"}
+                                        onClick={() => deleteTask(task.id)}>Deletar</Button>
+                            </div>
                         </div>
                     </ListGroup.Item>
                 ))}
             </ListGroup>
 
-
         </Container>
     )
-
 }
 
 export default TaskList;
